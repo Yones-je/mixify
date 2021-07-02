d3e9e9f284544d2268242bf705d10053ee056109
Yones-je <59484418+Yones-je@users.noreply.github.com>
2021-07-02 09:42:12 +0200

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
-import { Nav, Cabinet } from './components';
-import './App.css';
+import { Nav, Cabinet, Showcase } from './components';
+import './style/app.scss';
 
 const App = () => {
   const [openCabinet, setOpenCabinet] = useState(false);
+  const [drinkList, setDrinkList] = useState([]);
+
   return (
     <div className="App">
       <Nav openCabinet={openCabinet} setOpenCabinet={setOpenCabinet} />
-      <Cabinet openCabinet={openCabinet} />
+      <Cabinet
+        openCabinet={openCabinet}
+        setDrinkList={setDrinkList}
+        setOpenCabinet={setOpenCabinet}
+      />
+      <div className="showcase__container">
+        {drinkList !== 'None' ? (
+          <Showcase drinkList={drinkList} />
+        ) : (
+          'No drinks found for that combination, try something else!'
+        )}
+      </div>
     </div>
   );
 };
 
 export default App;
+
+/* drinkList.map(drink => (
+  <Showcase key={drink.idDrink} drink={drink} />
+)) */
---
+++ client/src/util.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const ingredientUrl = '/api/ingredients/';
+const drinkUrl = '/api/drinks/';
+
+export const getSuggestions = async query => {
+  try {
+    const response = await axios.get(`${ingredientUrl}${query}`);
+    const data = await response.data;
+    return data;
+  } catch (err) {
+    console.log(err.response.body);
+  }
+};
+
+export const requestFormatter = (arr, nonAlc) => {
+  const array = arr
+    .filter(item => item.selected === true)
+    .map(item => {
+      if (item.name.indexOf(' ') !== -1) {
+        return item.name.replace(' ', '_');
+      }
+      return item.name.trim();
+    });
+  const arrayString = array.join(',');
+  return nonAlc ? `${arrayString}&?a=Non_Alcoholic` : arrayString;
+};
+
+export const getDrink = async id => {
+  try {
+    const response = await axios.get(`${drinkUrl}${id}`);
+    const data = await response.data;
+    return data;
+  } catch (err) {
+    console.log(err.response.body);
+  }
+};
