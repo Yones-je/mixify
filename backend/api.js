08d3d96aa507756b301645c06673c95caac7458f
Yones-je <59484418+Yones-je@users.noreply.github.com>
2021-07-02 09:39:42 +0200

    Add files via upload

--- backend/api.js
+++ backend/api.js
@@ -1,27 +1,22 @@
-// ingredients api
-// filter by ingredient & non alcoholic:
-// http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Orange&a=Non_Alcoholic
-// lookup drink by id:
-//
 const { default: axios } = require("axios");
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 
 const api = express.Router();
-
+api.use(express.json());
 const apiKey = process.env.API_KEY;
 
 // Get drinks from multiple ingredients
 api.get("/ingredients/:ingredients", async (req, res) => {
   const { ingredients } = req.params;
-  console.log(ingredients, apiKey);
   try {
     const response = await axios(
       `https://thecocktaildb.com/api/json/v2/${apiKey}/filter.php?i=${ingredients}`
     );
-    const drinkList = response.data.drinks;
-    res.send(drinkList);
+    const drinkList = await response.data.drinks;
+    console.log(drinkList);
+    res.json(drinkList);
   } catch (err) {
     console.log(err);
     res.status(404).send(`No drinks with your ingredients found.`);
@@ -36,7 +31,7 @@ api.get("/drinks/:id", async (req, res) => {
       `https://www.thecocktaildb.com/api/json/v2/${apiKey}/lookup.php?i=${id}`
     );
     const drink = response.data.drinks;
-    res.send(drink);
+    res.json(drink);
   } catch {
     res.status(404).send(`No drink with id ${id} found.`);
   }
--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ const api = require("./api");
 
 const app = express();
 
-app.use(express.json());
 app.use("/api", api);
 
 module.exports.app = app;
