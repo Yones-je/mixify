b5eb0a6e7056df0220eae17b9b96cee308ec3d5e
Yones-je <59484418+Yones-je@users.noreply.github.com>
2021-08-03 16:15:31 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,30 @@
-import React, { useState } from 'react';
-import { Nav, Cabinet, Showcase, Trending, Placeholder } from './components';
-import './style/app.scss';
+import React, { useState } from "react";
+import { Nav, Cabinet, Showcase, Trending, Placeholder } from "./components";
+import "./style/app.scss";
 
 const App = () => {
-  const [openCabinet, setOpenCabinet] = useState(false);
-  const [drinkList, setDrinkList] = useState([]);
-  const [show, setShow] = useState(false);
+    const [drinkList, setDrinkList] = useState([]);
+    const [show, setShow] = useState(false);
 
-  return (
-    <div className="App">
-      <Nav
-        openCabinet={openCabinet}
-        setOpenCabinet={setOpenCabinet}
-        setShow={setShow}
-        show={show}
-      />
-      <Cabinet
-        openCabinet={openCabinet}
-        setDrinkList={setDrinkList}
-        setShow={setShow}
-        setOpenCabinet={setOpenCabinet}
-      />
-      {show ? (
-        <div className="showcase__container">
-          <Trending show={show} />
+    return (
+        <div className='App'>
+            <Nav setShow={setShow} show={show} />
+            <Cabinet setDrinkList={setDrinkList} setShow={setShow} />
+            {show ? (
+                <div className='showcase__container'>
+                    <Trending show={show} />
+                </div>
+            ) : (
+                <div className='showcase__container'>
+                    {drinkList && drinkList !== "None" ? (
+                        <Showcase drinkList={drinkList} />
+                    ) : (
+                        <Placeholder />
+                    )}
+                </div>
+            )}
         </div>
-      ) : (
-        <div className="showcase__container">
-          {drinkList && drinkList !== 'None' ? (
-            <Showcase drinkList={drinkList} />
-          ) : (
-            <Placeholder />
-          )}
-        </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default App;
